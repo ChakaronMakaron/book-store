1c36738245b3d3432e50f94d642b55e73983f9ca
Evgeniy Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2023-08-13 22:07:16 +0300

    book status change logic now reading from request body

--- src/main/java/com/andersen/controllers/servlet/BookControllerServlet.java
+++ src/main/java/com/andersen/controllers/servlet/BookControllerServlet.java
@@ -3,24 +3,31 @@ package com.andersen.controllers.servlet;
 import com.andersen.annotations.Get;
 import com.andersen.annotations.Put;
 import com.andersen.controllers.BookController;
+import com.andersen.controllers.servlet.jsonBodies.JsonBookStatusChange;
 import com.andersen.enums.BookSortKey;
 import com.andersen.models.Book;
 import com.andersen.services.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Singleton
 public class BookControllerServlet implements BookController {
 
     private final BookService bookService;
+    private final ObjectMapper objectMapper;
 
     @Inject
-    public BookControllerServlet(BookService bookService) {
+    public BookControllerServlet(BookService bookService, ObjectMapper objectMapper) {
         this.bookService = bookService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -40,11 +47,14 @@ public class BookControllerServlet implements BookController {
     @Override
     @Put("/books/change-status")
     public void changeBookStatus(HttpServletRequest request, HttpServletResponse response) {
-        Long id = Long.valueOf(request.getParameter("id"));
+        try {
+            String data = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+            JsonBookStatusChange statusChange = objectMapper.readValue(data, JsonBookStatusChange.class);
 
-        Book.BookStatus status = Book.BookStatus.valueOf(request.getParameter("status").toUpperCase());
-
-        bookService.changeBookStatus(id, status);
+            bookService.changeBookStatus(statusChange.getId(), statusChange.getStatus());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
